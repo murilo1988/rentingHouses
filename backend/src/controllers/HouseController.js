fde34a291637fb085fe43756d623f0efa1f4f309
Murilo Cabreira <murilocabreira@hotmail.com>
2023-04-04 10:24:14 -0300

    Criando url para thumbnail, criando cadastro de casa em HouseController

--- backend/src/controllers/HouseController.js
+++ backend/src/controllers/HouseController.js
@@ -1,8 +1,22 @@
+import House from "../models/House";
+
 class houseController {
-  store(req, res) {
+  async store(req, res) {
+    const { filename } = req.file;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
     console.log(req.body);
     console.log(req.file);
-    return res.json({ ok: true });
+    const house = await House.create({
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status,
+    });
+
+    return res.json(house);
   }
 }
 
